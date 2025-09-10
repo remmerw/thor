6388dbc42f07c3cdd2b2c8a78e1655327df9df56
remmerw <remmer.wilts@gmail.com>
2025-09-10 17:18:31 +0200

    Version 0.1.7 ------------- Cleanup

--- thor/src/commonMain/kotlin/io/github/remmerw/thor/ui/Table.kt
+++ thor/src/commonMain/kotlin/io/github/remmerw/thor/ui/Table.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import io.github.remmerw.saga.Entity
 import io.github.remmerw.thor.model.StateModel
@@ -21,8 +24,8 @@ fun Table(
     stateModel: StateModel,
     modifier: Modifier
 ) {
-    Column(modifier = Modifier.padding(8.dp)) {
-        ColumnEntities(entity, stateModel, modifier)
+    Column {
+        Entities(entity, stateModel, modifier)
     }
 }
 
@@ -33,11 +36,12 @@ fun Caption(
     stateModel: StateModel,
     modifier: Modifier
 ) {
-    Row(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+    Row(modifier = Modifier.padding(4.dp).fillMaxWidth()) {
         Entities(
             entity = entity,
             stateModel = stateModel,
-            modifier = modifier,
+            modifier = modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
             style = MaterialTheme.typography.labelLarge
         )
@@ -75,29 +79,61 @@ fun THead(
 
 
 @Composable
-fun Th(
-    entity: Entity,
-    stateModel: StateModel,
-    modifier: Modifier
-) {
-    Entities(entity, stateModel, modifier)
-}
-
-@Composable
 fun Tr(
     entity: Entity,
     stateModel: StateModel,
     modifier: Modifier
 ) {
-    Row(
-        modifier = Modifier.padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
-    ) {
-        Entities(entity, stateModel, modifier)
+
+    val entities by stateModel.children(entity).collectAsState(emptyList())
+
+    if (entities.isNotEmpty()) {
+
+        val weight = 1F
+
+        Row(
+            modifier = Modifier.padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+
+
+            entities.forEach { entity ->
+                Column(modifier = modifier.weight(weight, true)) {
+                    EntityComposable(
+                        entity = entity,
+                        stateModel = stateModel,
+                        modifier = modifier,
+                        /* color = color,
+                         fontSize = fontSize,
+                         fontStyle = fontStyle,
+                         textDecoration = textDecoration,
+                         textAlign = textAlign,
+                         fontWeight = fontWeight,
+                         style = style*/
+                    )
+                }
+            }
+        }
     }
 }
 
+
+@Composable
+fun Th(
+    entity: Entity,
+    stateModel: StateModel,
+    modifier: Modifier
+) {
+    Entities(
+        entity = entity,
+        stateModel = stateModel,
+        modifier = modifier.padding(4.dp),
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        style = MaterialTheme.typography.labelSmall
+    )
+}
 
 @Composable
 fun Td(
@@ -105,5 +141,10 @@ fun Td(
     stateModel: StateModel,
     modifier: Modifier
 ) {
-    Entities(entity, stateModel, modifier)
+    Entities(
+        entity = entity,
+        stateModel = stateModel,
+        modifier = modifier.padding(4.dp),
+        textAlign = TextAlign.Left
+    )
 }
