5a846407eefd95ad26b84b36060790187ef8baad
remmerw <remmer.wilts@gmail.com>
2025-09-10 16:24:48 +0200

    Version 0.1.7 ------------- Cleanup

--- thor/src/commonTest/kotlin/io/github/remmerw/thor/FormTest.kt
+++ thor/src/commonTest/kotlin/io/github/remmerw/thor/FormTest.kt
@@ -2,7 +2,6 @@ package io.github.remmerw.thor
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.lifecycle.viewmodel.compose.viewModel
-import io.github.remmerw.saga.attachToModel
 import io.github.remmerw.thor.model.StateModel
 import io.github.remmerw.thor.ui.HtmlViewer
 import kotlinx.coroutines.Dispatchers
@@ -48,7 +47,7 @@ class FormTest {
 
             val buffer = Buffer()
             buffer.write(data.encodeToByteArray())
-            attachToModel(buffer, model)
+            stateModel.parse(buffer)
 
             model.debug()
         }
